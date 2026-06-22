863ab46ac5a262e5d78876ca7197935b91ba1d78
bilal lafdili <bilallafdili23@gmail.com>
2026-06-22 16:17:25 +0200

    connection to db

--- src/main/java/team6/Application.java
+++ src/main/java/team6/Application.java
@@ -1,6 +1,10 @@
 package team6;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("u4-buildweek-team6-pu");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
