b7d8da841db89dd847483c559ffcaa82a95c6d39
bilal lafdili <bilallafdili23@gmail.com>
2026-06-23 15:49:10 +0200

    ...

--- src/main/java/team6/dao/PercorrenzaDao.java
+++ src/main/java/team6/dao/PercorrenzaDao.java
@@ -81,7 +81,6 @@ public class PercorrenzaDao {
         return query.getSingleResult(); // restituisce il numero long di viaggi fatti
     }
 
-<<<<<<< HEAD
     // calcolo dell media percorrenza di una tratta attraverso un id tratta
     public Double calcolaTempoMedioEffettivo(long idTratta) {
         List<Percorrenza> percorrenze = em
@@ -97,6 +96,5 @@ public class PercorrenzaDao {
                 .mapToInt(Percorrenza::getPercorrenzaEffettiva).average().orElseThrow();
     }
 
-=======
->>>>>>> develop
+
 }
